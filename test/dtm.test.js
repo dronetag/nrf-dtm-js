909de026dfef24917a1f8d44d8ee452772c53af5
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2019-04-03 17:16:14 +0200

    Created tests for DTM

--- test/dtm.test.js
+++ test/dtm.test.js
@@ -41,13 +41,14 @@
 const DTM = require('../dist/nrf-dtm');
 
 const dtm = new DTM.DTM('COM31');
+const dtmReceiver = new DTM.DTM('COM27');
 /*
 describe('DTM utility test', () => {
     it('Frequency should be 000000', () => {
         expect(DTM.DTM_FREQUENCY(2402)).toBe('000000');
         expect(DTM.DTM_FREQUENCY(2480)).toBe('100111');
     });
-    it(`Command format should be ['0F', '0F']`, () => {
+    it('Command format should be ['0F', '0F']', () => {
         expect(DTM.DTM_CMD_FORMAT('0000111100001111')).toEqual(Buffer.from(['0x0f', '0x0f']));
     });
 
@@ -55,85 +56,111 @@ describe('DTM utility test', () => {
 */
 
 describe('Command test', () => {
-    it(`Setup command should be ['0x00', '0x00']`, () => {
+    it('Setup command should be [\'0x00\', \'0x00\']', () => {
         expect(dtm.dtmTransport.createSetupCMD()).toEqual(Buffer.from(['0x00', '0x00']));
     });
 
-    it(`Transmitter command should be ['0x80', '0x00']`, () => {
+    it('Transmitter command should be [\'0x80\', \'0x00\']', () => {
         expect(dtm.dtmTransport.createTransmitterCMD()).toEqual(Buffer.from(['0x80', '0x00']));
     });
 
-    it(`Receiver command should be ['0x40', '0x00']`, () => {
+    it('Receiver command should be [\'0x40\', \'0x00\']', () => {
         expect(dtm.dtmTransport.createReceiverCMD()).toEqual(Buffer.from(['0x40', '0x00']));
     });
 
-    it(`End command hould be ['0xC0', '0x00']`, () => {
+    it('End command hould be [\'0xC0\', \'0x00\']', () => {
         expect(dtm.dtmTransport.createEndCMD()).toEqual(Buffer.from(['0xc0', '0x00']));
     });
 });
 
 describe('Sending command test', () => {
-    it(`Return value should be ['0x00', '0x00']`, async () => {
+    it('Return value should be [\'0x00\', \'0x00\']', async () => {
         expect(await dtm.dtmTransport.sendCMD(dtm.dtmTransport.createSetupCMD())).toEqual(Buffer.from(['0x00', '0x00']));
     });
-    it(`Return value should be ['0x00', '0x00']`, async () => {
+    it('Return value should be [\'0x00\', \'0x00\']', async () => {
         expect(await dtm.dtmTransport.sendCMD(dtm.dtmTransport.createTransmitterCMD())).toEqual(Buffer.from(['0x00', '0x00']));
     });
-    it(`Return value should be ['0x00', '0x00']`, async () => {
+    it('Return value should be [\'0x00\', \'0x00\']', async () => {
         expect(await dtm.dtmTransport.sendCMD(dtm.dtmTransport.createEndCMD())).toEqual(Buffer.from(['0x80', '0x00']));
     });
 });
 
 describe('Setup test', () => {
-    it(`Setup reset succeeds`, async () => {
+    it('Setup reset succeeds', async () => {
         expect(await dtm.setupReset()).toEqual(Buffer.from(['0x00', '0x00']));
     });
 
-    it(`Setup short packet length`, async () => {
+    it('Setup short packet length', async () => {
         expect(await dtm.setupLength(0x01)).toEqual(Buffer.from(['0x00', '0x00']));
     });
-    it(`Setup long packet length`, async () => {
+    it('Setup long packet length', async () => {
         expect(await dtm.setupLength(0x40)).toEqual(Buffer.from(['0x00', '0x00']));
     });
-    it(`Setup packet lengths longer than 0xFF will fail`, async () => {
+    it('Setup packet lengths longer than 0xFF will fail', async () => {
         expect(await dtm.setupLength(0x100)).toEqual(Buffer.from(['0x00', '0x01']));
     });
 
-    it(`Uncoded phy LE is supported`, async () => {
+    it('Uncoded phy LE is supported', async () => {
         expect(await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_2M)).toEqual(Buffer.from(['0x00', '0x00']));
     });
 
-    it(`Coded phy LE is supported on nRF52840`, async () => {
+    it('Coded phy LE is supported on nRF52840', async () => {
         expect(await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_CODED_S8)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_CODED_S2)).toEqual(Buffer.from(['0x00', '0x00']));
     });
 
-    it(`Setup modulation index`, async () => {
+    it('Setup modulation index', async () => {
         expect(await dtm.setupModulation(dtm.DTM_PARAMETER.STANDARD_MODULATION_INDEX)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setupModulation(dtm.DTM_PARAMETER.STABLE_MODULATION_INDEX)).toEqual(Buffer.from(['0x00', '0x00']));
     });
 
-    it(`Read features`, async () => {
+    it('Read features', async () => {
         const readFeatures = async () => {
-            const buffer = await dtm.setupReadFeatures()
+            const buffer = await dtm.setupReadFeatures();
             buffer[0] &= 0x01; // LE_Test_Status_Event
             buffer[1] &= 0x80; // Success
-            return buffer
+            return buffer;
         };
         expect(await readFeatures()).toEqual(Buffer.from(['0x00', '0x00']));
     });
 
-    it(`Set TX power`, async () => {
+    it('Set TX power', async () => {
+        expect(await dtm.setupReset()).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setTxPower(-40)).toEqual(Buffer.from(['0x00', '0x00']));
-        expect(await dtm.setTxPower(-80)).toEqual(Buffer.from(['0x00', '0x01']));
+        expect(await dtm.setTxPower(-20)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(-16)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(-12)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(-8)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setTxPower(-4)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setTxPower(0)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(2)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(3)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setTxPower(4)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(5)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(6)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(7)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.setTxPower(8)).toEqual(Buffer.from(['0x00', '0x00']));
         expect(await dtm.setTxPower(9)).toEqual(Buffer.from(['0x00', '0x01']));
+        expect(await dtm.setTxPower(1)).toEqual(Buffer.from(['0x00', '0x01']));
+        expect(await dtm.setTxPower(-41)).toEqual(Buffer.from(['0x00', '0x01']));
     });
+
+/* This test will make other test fail.
+    it('Select timer', async () => {
+        expect(await dtm.setupReset()).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.selectTimer(0)).toEqual(Buffer.from(['0x00', '0x00']));
+        expect(await dtm.selectTimer(1)).toEqual(Buffer.from(['0x00', '0x00']));
+
+            //Select timer 2 doesn't return. Test will time out. ->
+
+            //expect(await dtm.selectTimer(2)).toEqual(Buffer.from(['0x00', '0x00']));
+
+    });*/
+
+
 /*
-    it(`Read TXRX`, async () => {
+    it('Read TXRX', async () => {
         const readSupportedRxTx = async param => {
             const buffer = await dtm.setupReadSupportedRxTx(param)
             buffer[0] &= 0x01; // LE_Test_Status_Event
@@ -143,4 +170,96 @@ describe('Setup test', () => {
         expect(await readSupportedRxTx(dtm.DTM_PARAMETER.SUPPORTED_MAX_TX_TIME)).toEqual(Buffer.from(['0x00', '0x00']));
     });
 */
+});
+
+
+describe('Transmit and receive tests', () => {
+    it('Transmit and receive single channel', async () => {
+        await dtm.setupReset();
+        await dtmReceiver.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M)
+        const transmitter = dtm.singleChannelTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 20, 0);
+        const recv = await dtmReceiver.singleChannelReceiverTest(20, 2000);
+        dtm.endTest();
+        await transmitter;
+        expect(recv.success).toEqual(true);
+        expect(recv.received).toBeGreaterThan(1000);
+    });
+
+    it('Transmit on one channel. Receiver sweep on 5.', async () => {
+        await dtm.setupReset();
+        await dtmReceiver.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M)
+        const transmitter = dtm.singleChannelTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 20, 0);
+        const recv = await dtmReceiver.sweepReceiverTest(18, 23, 50, 2000);
+        dtm.endTest();
+        await transmitter;
+        expect(recv.success).toEqual(true);
+        expect(recv.received).toBeGreaterThan(200);
+    });
+
+    it('Transmit on 5 channels. Receive on single channel.', async () => {
+        await dtm.setupReset();
+        await dtmReceiver.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M)
+        const transmitter = dtm.sweepTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 18, 23, 50, 0);
+        const recv = await dtmReceiver.singleChannelReceiverTest(20, 2000);
+        dtm.endTest();
+        await transmitter;
+        expect(recv.success).toEqual(true);
+        expect(recv.received).toBeGreaterThan(200);
+    });
+
+    it('Transmit on 5 channels. Receive on 5 channels.', async () => {
+        await dtm.setupReset();
+        await dtmReceiver.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M);
+        const transmitter = dtm.sweepTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 18, 23, 100, 0);
+        const recv = await dtmReceiver.sweepReceiverTest(18, 23, 15, 2000);
+        dtm.endTest();
+        await transmitter;
+        expect(recv.success).toEqual(true);
+        expect(recv.received).toBeGreaterThan(100);
+    });
+
+    it('Random sweep', async () => {
+        await dtm.setupReset();
+        await dtmReceiver.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M);
+        const transmitter = dtm.sweepTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 0, 39, 20, 0, true);
+        const recv = await dtmReceiver.sweepReceiverTest(0, 39, 20, 3000, true);
+        dtm.endTest();
+        await transmitter;
+        expect(recv.success).toEqual(true);
+        expect(recv.received).toBeGreaterThan(0);
+    });
+
+    it('Non overlapping sequential sweeps returns no packets.', async () => {
+        await dtm.setupReset();
+        await dtmReceiver.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M);
+        const transmitter = dtm.sweepTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 0, 20, 20, 0);
+        const recv = await dtmReceiver.sweepReceiverTest(21, 39, 20, 3000);
+        dtm.endTest();
+        await transmitter;
+        expect(recv.success).toEqual(true);
+        expect(recv.received).toEqual(0);
+    });
+
+    it('Perform 30 sweepTransmitterTests', async () => {
+        await dtm.setupReset();
+        await dtm.setupPhy(dtm.DTM_PARAMETER.PHY_LE_1M);
+        for (let i = 0; i < 30; i++) {
+            const response = await dtm.sweepTransmitterTest(dtm.DTM_PKT.PAYLOAD_PRBS9, 1, 0, 39, 20, 100);
+            expect(response.success).toEqual(true);
+        }
+    });
+
+    it('Perform 30 sweepReceiverTests', async () => {
+        await dtm.setupReset();
+        for (let i = 0; i < 30; i++) {
+            const response = await dtm.sweepReceiverTest(0, 39, 20, 100);
+            expect(response.success).toEqual(true);
+        }
+    });
 });
